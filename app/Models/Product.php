17bfe7163a56f3c27ef47675cad0ad204916fa8f
sanaz1992 <sanaz.keyvanloo1992@gmail.com>
2025-03-15 12:50:06 +0330

    add user_id to products and define relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,11 +8,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Product extends Model
 {
     // protected $table="products";
-    protected $fillable = ['name', 'image', 'description'];
+    protected $fillable = ['name', 'image', 'description', 'category_id', 'slug', 'price', 'stock', 'user_id'];
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 
     public function orders(): BelongsToMany
