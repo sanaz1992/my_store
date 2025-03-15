ada4f322be51f8e0c933171984b5d1ba15f2070b
sanaz1992 <sanaz.keyvanloo1992@gmail.com>
2025-03-15 12:45:15 +0330

    create models and migrations

---
+++ database/migrations/2025_03_15_091029_add_cols_to_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('slug');
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('category_id');
+            $table->dropColumn('slug');
+            $table->dropColumn('price');
+            $table->dropColumn('stock');
+        });
+    }
+};
